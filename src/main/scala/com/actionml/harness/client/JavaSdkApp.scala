db443769b60c9876794b7dd4424ddbd8c864fb23
Andrey Shikov <andrey.shikov@gmail.com>
2019-03-05 21:33:56 +0300

    Added retries for client

--- src/main/scala/com/actionml/harness/client/JavaSdkApp.scala
+++ src/main/scala/com/actionml/harness/client/JavaSdkApp.scala
@@ -5,9 +5,12 @@ import java.util.concurrent.CompletionStage
 
 import akka.NotUsed
 import akka.actor.ActorSystem
-import akka.http.javadsl.model.{ HttpResponse, StatusCodes }
+import akka.http.javadsl.model.{ HttpResponse, StatusCode, StatusCodes }
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.marshalling.Marshal
+import akka.http.scaladsl.model.MediaTypes.`application/json`
+import akka.http.scaladsl.model.{ HttpMethods, HttpRequest, RequestEntity }
 import akka.stream.Supervision.Directive
-import akka.stream.javadsl.JavaFlowSupport.Source
 import akka.stream.scaladsl.{ FileIO, Flow, Framing }
 import akka.stream.{ ActorMaterializer, ActorMaterializerSettings, Supervision }
 import akka.util.ByteString
@@ -15,13 +18,15 @@ import com.actionml.EventsClient
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
+import scala.concurrent.duration._
 import scala.concurrent.java8.FuturesConvertersImpl._
+import scala.util.{ Failure, Success }
 
 object JavaSdkApp {
   def main(args: Array[String]): Unit =
     RunArgs.parse(args) match {
-      case Some(a @ RunArgs(_, _, _, _, _, true, _)) =>
-        val system = ActorSystem.apply("harness-load-test");
+      case Some(a @ RunArgs(parallelism, _, _, _, _, true, _)) =>
+        implicit val system = ActorSystem.apply("harness-load-test")
         val decider: akka.japi.function.Function[Throwable, Supervision.Directive] =
           new akka.japi.function.Function[Throwable, Supervision.Directive]() {
             override def apply(e: Throwable): Directive = {
@@ -32,31 +37,45 @@ object JavaSdkApp {
         implicit val materializer =
           ActorMaterializer.create(ActorMaterializerSettings.create(system).withSupervisionStrategy(decider), system)
 
-        val eventsClient = new EventsClient(a.engineId, a.harnessHost, a.harnessPort)
         FileIO
           .fromPath(Paths.get(a.fileName))
-          .via(Framing.delimiter(ByteString("\n"), 2048, allowTruncation = false).map(_.utf8String))
-          .via(sendFlow(eventsClient))
-          .async
-          .runFold(0)((acc, _) => {
-            if (acc % 10000 == 0) println(acc)
-            acc + 1
+          .via(Framing.delimiter(ByteString("\n"), 4096, allowTruncation = false).map(_.utf8String))
+          .mapAsync(a.nThreads)(e => {
+            Marshal(e).to[RequestEntity].map { entity =>
+              HttpRequest(
+                method = HttpMethods.POST,
+                uri = s"http://${a.harnessHost}:${a.harnessPort}/engines/${a.engineId}/events",
+                entity = entity.withContentType(`application/json`)
+              ) -> e
+            }
           })
+          .via(Http().cachedHostConnectionPool[String](a.harnessHost, a.harnessPort))
+          .throttle(10, 1.second)
+          .runFold(0) {
+            case (acc, (Success(resp), event)) =>
+              if (resp.status != StatusCodes.CREATED) println(s"WRONG STATUS CODE for event $event")
+              if (acc % 100000 == 0) println(acc)
+              acc + 1
+            case (acc, (Failure(e), event)) =>
+              println(s"ERROR ${e.getMessage} for event $event")
+              e.printStackTrace
+              acc + 1
+          }
           .foreach(println)
       case _ =>
         System.exit(1)
     }
 
-  def sendFlow(client: EventsClient): Flow[String, String, NotUsed] =
+  def sendFlow(client: EventsClient): Flow[String, StatusCode, NotUsed] =
     Flow[String]
       .flatMapConcat { s =>
         client
           .createRaw(s)
           .map {
-            new akka.japi.function.Function[HttpResponse, String]() {
-              override def apply(r: HttpResponse): String = {
+            new akka.japi.function.Function[HttpResponse, StatusCode]() {
+              override def apply(r: HttpResponse): StatusCode = {
                 if (r.status != StatusCodes.CREATED) println(s"ERROR: wrong status code ${r.status}")
-                s
+                r.status
               }
             }
           }
